7baef85996ee6ea0597af866e871cbf7c1d66239
Gonzo <dggsax@gmail.com>
2017-06-27 00:16:38 -0400

    Turned the server into a resful server

--- server.py
+++ server.py
@@ -1,65 +1,26 @@
-import time
-import math
-from threading import Thread, Lock
-from flask import Flask, render_template, session, request
-from flask_socketio import SocketIO, emit, join_room, leave_room,close_room, rooms, disconnect
+from flask import Flask, render_template, request, url_for, jsonify, send_from_directory
+from flaskext.mysql import MySQL
+from flask_restful import Resource, Api
 
-# import logging
-# log = logging.getLogger('werkzeug')
-# log.setLevel(logging.ERROR)
+app = Flask(__name__)
+api = Api(app)
 
-async_mode = None
-if async_mode is None:
-    try:
-        import eventlet
-        async_mode = 'eventlet'
-    except ImportError:
-        pass
+#mysql = MySQL()
+#app.config['MYSQL_DATABASE_USER'] = '6s08'
+#app.config['MYSQL_DATABASE_PASSWORD'] = 'dome-crawler'
+#app.config['MYSQL_DATABASE_DB'] = '6s08'
+#app.config['MYSQL_DATABASE_HOST'] = 'domecrawl.us'
+#mysql.init_app(app)
 
-    if async_mode is None:
-        try:
-            from gevent import monkey
-            async_mode = 'gevent'
-        except ImportError:
-            pass
+@app.route("/")
+def hello():
+    return send_from_directory("static", "index.html")
 
-    if async_mode is None:
-        async_mode = 'threading'
-
-    print('async_mode is ' + async_mode)
-
-# monkey patching is necessary because this application uses a background
-# thread
-if async_mode == 'eventlet':
-    import eventlet
-    eventlet.monkey_patch()
-elif async_mode == 'gevent':
-    from gevent import monkey
-    monkey.patch_all()
-
-#Start up Flask server:
-app = Flask(__name__, template_folder = './',static_url_path='/static')
-app.config['SECRET_KEY'] = 'secret!' #shhh don't tell anyone. Is a secret
-socketio = SocketIO(app, async_mode = async_mode)
-thread = None
+@app.route("/<path:path>")
+def open_file(path):
+    return send_from_directory("static", path)
 
 
-def dataThread():
-    pass
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=4999, threaded=True)
 
-@app.route('/')
-def index():
-    global thread
-    print ("A user connected")
-    if thread is None:
-        thread = Thread(target=dataThread)
-        thread.daemon = True
-        thread.start()
-    return render_template('pages/index.html')
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=4999, debug=True)
-
-
-
-    
