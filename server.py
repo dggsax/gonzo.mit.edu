b191b9d2b2384e8afb0a0c90f84a4f6597b3732c
Gonzo <dggsax@gmail.com>
2017-06-26 23:59:30 -0400

    updated server file

--- server.py
+++ server.py
@@ -58,7 +58,7 @@ def index():
     return render_template('pages/index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, port=5000, debug=True)
+    socketio.run(app, port=0.0.0.0, debug=True)
 
 
 
